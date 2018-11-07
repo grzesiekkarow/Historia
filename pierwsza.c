cb1de7cab00f7b8e0eb319a5cf7789b76c0646ac
grzesiekkarow <44462067+grzesiekkarow@users.noreply.github.com>
2018-11-07 19:10:51 +0100

    Add files via upload
    niedzialajacy kod

---
+++ pierwsza.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>//biblioteka potrzebna bo u¿ywam funkcji sqrt (pierwiastek kwadratowy)
+int main()
+{
+    int liczba,i;
+    do
+    {
+        printf("Podaj liczbe z <0,100>\n");
+        //fflush(stdin);//czyszczenie bufora
+        scanf("%d",&liczb);
+    }while(lizba<0 || lczba>100);
+    for (i=2;i<=sqrt(liczba);i++)//optymalizacja
+    //for (i=2;i<liczba);i++) mo¿na tak
+    {
+        if (liczba%i==0)
+        {
+            printf("Nie jest to liczba pierwsza! \n");
+            getchar();
+            return 0;
+        }
+    }
+    printf("Liczba pierwsza:\n");
+    getchar();
+    return 0;
+}
